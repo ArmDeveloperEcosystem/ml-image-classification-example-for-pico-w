99c39bbfaaf36da49daba5985db13d03a38d9a62
Sandeep Mistry <sandeep.mistry@arm.com>
2023-11-28 16:31:16 -0500

    Integrate Wi-Fi, Slack client, exponential smoothing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
+#include <pico/cyw43_arch.h>
 
 #include <bsp/board.h>
 #include <tusb.h>
@@ -12,6 +13,8 @@
 
 extern "C" {
     #include "pico/hm01b0.h"
+    #include "logging.h"
+    #include "slack_client.h"
 }
 
 #include "image_classifier.h"
@@ -41,8 +44,14 @@ SemaphoreHandle_t tensor_arena_mutex;
 uint8_t tensor_arena[101 * 1024];
 ImageClassifier image_classifier(tensor_arena, sizeof(tensor_arena));
 
+char slack_client_buf[2048];
+slack_client_t slack_client;
+
 void main_task(void*);
 void usb_task(void*);
+
+bool occupied = false;
+float smoothed_occupied_prediction = 0.0;
 
 int main(void)
 {
@@ -55,7 +64,7 @@ int main(void)
     tensor_arena_mutex = xSemaphoreCreateMutex();
 
     xTaskCreate(main_task, "MainTask", 2 * 1024, NULL, (tskIDLE_PRIORITY + 1), NULL);
-    xTaskCreate(usb_task, "USBTask", 2 * 1024, NULL, (tskIDLE_PRIORITY + 1), NULL);
+    xTaskCreate(usb_task, "USBTask", 1 * 1024, NULL, (tskIDLE_PRIORITY + 1), NULL);
     vTaskStartScheduler();
 
     return 0;
@@ -67,17 +76,42 @@ void main_task(void*)
         taskYIELD();
     }
 
-    printf("hello\n");
+    LogInfo(("Hello"));
 
     if (hm01b0_init(&hm01b0_config) != 0) {
-        printf("failed to initialize camera!\n");
+        LogError(("Failed to initialize camera!\n"));
         return;
     }
 
+    // lower the exposure
+    // hm01b0_set_coarse_integration(2);
+
     if (!image_classifier.init()) {
-        printf("failed to initialize image classifier!\n");
+        LogError(("Failed to initialize image classifier!\n"));
         return;
     }
+
+    if (cyw43_arch_init()) {
+        LogError(("Failed to initialize Wi-Fi!"));
+        return;
+    }
+
+    cyw43_arch_enable_sta_mode();
+
+    LogInfo(("Connecting to Wi-Fi SSID '%s'", WIFI_SSID));
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        LogError(("Failed to connect to Wi-Fi SSID '%s' !", WIFI_SSID));
+        return;
+    } else {
+        LogInfo(("Connected to Wi-Fi SSID '%s'", WIFI_SSID));
+    }
+
+     if (slack_client_init(&slack_client, SLACK_BOT_TOKEN, NULL, slack_client_buf, sizeof(slack_client_buf)) != 0) {
+        LogError(("Failed to initialize Slack client!"));
+        return;
+    }
+
+    slack_client_post_message(&slack_client, ":wave: Hello Slack", "pico-w-test");
 
     while (1) {
         xSemaphoreTake(tensor_arena_mutex, portMAX_DELAY);
@@ -86,7 +120,10 @@ void main_task(void*)
 
         float* predictions = image_classifier.predict(monochrome_buffer, FRAME_WIDTH, FRAME_HEIGHT);
 
-        printf("predictions = %f %f\n", predictions[0], predictions[1]);
+        LogInfo(("predictions = %f %f", predictions[0], predictions[1]));
+
+        smoothed_occupied_prediction *= 0.8;
+        smoothed_occupied_prediction += predictions[1] * 0.2;
 
         if (tud_video_n_streaming(0, 0)) {
             const uint8_t* src = monochrome_buffer;
@@ -103,13 +140,31 @@ void main_task(void*)
         } else {
             xSemaphoreGive(tensor_arena_mutex);
         }
+
+        if (smoothed_occupied_prediction > 0.5) {
+            if (!occupied) {
+                occupied = true;
+
+                LogInfo(("EV Charger is occupied"));
+
+                slack_client_post_message(&slack_client, "EV charger 1 is occupied :car:", "pico-w-test");
+            }
+        } else {
+            if (occupied) {
+                occupied = false;
+
+                LogInfo(("EV Charger is available"));
+
+                slack_client_post_message(&slack_client, "EV charger 1 is available", "pico-w-test");
+            }
+        }
     }
 }
 
 void usb_task(void*)
 {
     while (1) {
-        taskYIELD();
+        vTaskDelay(1);
 
         tud_task();
     }
